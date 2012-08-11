85ee140ae559774cecbe1a15b7db89c940a5ebd8
Vinhthuy Phan <vphan@memphis.edu>
2012-08-11 18:06:15 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Sal',
+    version='0.1.1',
+    author='Vinhthuy Phan',
+    author_email='vphan@memphis.edu',
+    packages=['sal'],
+    url='https://github.com/vtphan/sal',
+    license='MIT',
+    description='A storage abstraction layer.',
+    long_description='A storage abstraction layer.',
+    install_requires=[],
+)
