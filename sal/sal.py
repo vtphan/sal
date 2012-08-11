07e072470cbda00e2ce98fc8fe321cc765d04746
Vinhthuy Phan <vphan@memphis.edu>
2012-08-11 18:13:51 -0500

    Added sal, validators

---
+++ sal/sal.py
@@ -0,0 +1,380 @@
+import re
+import time
+import datetime
+import json
+import inspect
+from validator import *
+
+#--------------------------------------------------------------------
+# Types
+#--------------------------------------------------------------------
+
+class BaseType(object):
+	def __init__(self, t):
+		self.type = t
+
+	# return value of a string v, or object of the same type as self.type
+	def value(self, v):
+		if v==None: return None
+		try:
+			return self.type(v)
+		except:
+			raise TypeError('"%s" is invalid of type %s' % (v,self.type))
+
+	def serialize(self, v):
+		return str(v)
+
+	def __str__(self):
+		return str(self.type)
+
+class DatetimeType( BaseType ):
+	def __init__(self):
+		super(DatetimeType, self).__init__(datetime.datetime)
+
+	def value(self, v):
+		if v is None: return None
+		if isinstance(v, datetime.datetime): return v
+		if not isinstance(v, (str,unicode)):
+			raise TypeError('%s not an instance of Datetime' % str(v))
+		if '.' in v:
+			(y, m, d, hh, mm, ss, t0, t1, t2) = time.strptime(v,'%Y-%m-%d %H:%M:%S.%f')
+		else:
+			(y, m, d, hh, mm, ss, t0, t1, t2) = time.strptime(v,'%Y-%m-%d %H:%M:%S')
+		return datetime.datetime(y, m, d, hh, mm, ss)
+
+
+class DateType( BaseType ):
+	def __init__(self):
+		super(DateType, self).__init__(datetime.date)
+
+	def value(self, v):
+		if v is None: return None
+		if isinstance(v, datetime.date): return v
+		if not isinstance(v, (str,unicode)):
+			raise TypeError('%s not an instance of Date' % str(v))
+		(y, m, d, hh, mm, ss, t0, t1, t2) = time.strptime(v,'%Y-%m-%d')
+		return datetime.date(y, m, d)
+
+
+class TimeType( BaseType ):
+	REGEX_TIME = re.compile('((?P<h>[0-9]+))([^0-9 ]+(?P<m>[0-9 ]+))?([^0-9ap ]+(?P<s>[0-9]*))?\s?((?P<d>[ap]m))?')
+
+	def __init__(self):
+		super(TimeType, self).__init__(datetime.time)
+
+	def value(self, v):
+		if v is None: return None
+		if isinstance(v, datetime.time): return v
+		if not isinstance(v, (str,unicode)):
+			raise TypeError('%s not an instance of Time' % str(v))
+
+		''' Formats:
+			hh:mm:ss [am/pm]
+			hh:mm [am/pm]
+			hh [am/pm]
+		'''
+		value = TimeType.REGEX_TIME.match(v.lower())
+		(h, m, s) = (int(value.group('h')), 0, 0)
+		if value.group('m') is not None:
+			m = int(value.group('m'))
+		if value.group('s') is not None:
+			s = int(value.group('s'))
+		if value.group('d') == 'pm' and 0 < h < 12:
+			h = h + 12
+		return datetime.time(h,m,s)
+
+
+class IntType( BaseType ):
+	def __init__(self):
+		super(IntType,self).__init__(int)
+
+
+class LongType( BaseType ):
+	def __init__(self):
+		super(LongType,self).__init__(long)
+
+
+class FloatType( BaseType ):
+	def __init__(self):
+		super(FloatType,self).__init__(float)
+
+
+class BooleanType( BaseType ):
+	def __init__(self):
+		super(BooleanType,self).__init__(bool)
+
+	def value(self, v):
+		if v is None: return None
+		if v in ('True', True): return True
+		if v in ('False', False): return False
+		raise TypeError('%s not an instance of Boolean' % v)
+
+class VarType( BaseType ):
+	def __init__(self):
+		super(VarType,self).__init__(str)
+
+
+class VarcharType( BaseType ):
+	def __init__(self):
+		super(VarcharType,self).__init__(str)
+
+
+class TypeFactory(object):
+	_type = dict(int=IntType, long=LongType, float=FloatType,
+		boolean=BooleanType, var=VarType, varchar=VarcharType,
+		date=DateType, time=TimeType, datetime=DatetimeType)
+
+	@classmethod
+	def get(cls, t):
+		if t not in cls._type:
+			raise TypeError('unknown type %s' % t)
+		return cls._type[t]()
+
+##---------------------------------------------------------------------
+
+class InvalidField( Exception ):
+    def __init__(self, error, *args, **kwargs):
+        super(InvalidField, self).__init__(error, *args, **kwargs)
+        self.error = error
+
+    def __str__(self):
+        return '%s.' % (self.error,)
+
+
+#--------------------------------------------------------------------
+# EXPRESSIONS
+#--------------------------------------------------------------------
+
+class Expression(object):
+	''' Boolean expressions '''
+	def __init__(self, op, *operands):
+		self.op = op
+		self.operands = operands
+		self.field_name = None
+		self.model_name = None
+
+	# returns True of False
+	def eval(self):
+		p=[o.eval() if isinstance(o, Expression) else o for o in self.operands]
+		return self.op(*p)
+
+	def __str__(self):
+		params = ', '.join([str(o) for o in self.operands])
+		return '%s(%s)' % (self.op.name, params)
+
+	def __invert__(self):
+		return Expression(is_not(), self)
+
+	def __and__(self, other):
+		return Expression(is_both(), self, other)
+
+	def __or__(self, other):
+		return Expression(is_either(), self, other)
+
+
+	def to_sql(self):
+		if self.op.name in ('is_eq','is_ne','is_lt','is_le','is_gt','is_ge', \
+				'is_in', 'is_notin'):
+			if isinstance(self.operands[1], Field):
+				values = [self.operands[1].value]
+			else:
+				values = [self.operands[1]]
+			return (self.op.sql(self.field_name, '%s'), values)
+		if self.op.name in ('is_both', 'is_either'):
+			L, L_values = self.operands[0].to_sql()
+			R, R_values = self.operands[1].to_sql()
+			return (self.op.sql(L,R), L_values + R_values)
+		if self.op.name in ('is_not',):
+			R, values =	self.operands[0].to_sql()
+			return (self.op.sql(R), values)
+		return ('', [''])
+
+
+	def to_mongo(self):
+		if self.op.name in ('is_eq','is_ne','is_lt','is_le','is_gt','is_ge', \
+				'is_in', 'is_notin'):
+			return self.op.mongo(self.field_name, self.operands[1])
+		if self.op.name in ('is_both', 'is_either'):
+			L = self.operands[0].to_mongo()
+			R = self.operands[1].to_mongo()
+			return self.op.mongo(L,R)
+		if self.op.name in ('is_not',):
+			return self.op.mongo(self.operands[0].to_mongo())
+		return {}
+
+##---------------------------------------------------------------------
+class CUExpression ( Expression ):
+	''' Comparable unary expressions have only one operand.
+		The operand is comparable, not the expression itself, which is boolean
+	'''
+	def __init__(self, op, value):
+		super(CUExpression,self).__init__(op, value)
+
+	def set_value(self, v):
+		self.operands = [ Expression(is_value(), v, self.type) ]
+
+	def get_value(self):
+		return self.operands[0].eval()
+
+	value = property(get_value, set_value)
+
+	# Compare "value" of the expression, not the expression itself
+	def new_expression(self, op, other):
+		e = Expression(op, self.value, other)
+		e.field_name = self.field_name
+		e.model_name = self.model_name
+		return e
+
+	def __lt__(self, other):
+		return self.new_expression(is_lt(), other)
+
+	def __le__(self, other):
+		return self.new_expression(is_le(), other)
+
+	def __gt__(self, other):
+		return self.new_expression(is_gt(), other)
+
+	def __ge__(self, other):
+		return self.new_expression(is_ge(), other)
+
+	def __eq__(self, other):
+		return self.new_expression(is_eq(), other)
+
+	def __ne__(self, other):
+		return self.new_expression(is_ne(), other)
+
+	def is_in(self, other):
+		return self.new_expression(is_in(), other)
+
+
+class Field (CUExpression):
+	def __init__(self, field_type, *validators, **kw):
+		self.field_type = field_type
+		self.type = TypeFactory.get(field_type)
+		self.validators = validators
+		e = Expression(is_value(), kw.get('value',None), self.type)
+		super(Field,self).__init__(combine_validators(validators), e)
+
+	def serialize(self):
+		return self.type.serialize(self.value)
+
+	def __str__(self):
+		return self.value if self.value is not None else 'None'
+
+	def __repr__(self):
+		return '%s: %s' % (self.field_name, super(Field,self).__str__())
+
+
+class _Q (object):
+	def __getattribute__(self, name):
+		return Field('varchar', name)
+
+Q = _Q()
+
+##---------------------------------------------------------------------
+class field_property(object):
+	def __init__(self, field_name):
+		self.field_name = field_name
+
+	def __get__(self, obj, objtype):
+		if obj is not None:
+			return obj.fields[self.field_name]
+		else:
+			return objtype._fields[self.field_name]
+
+	def __set__(self, obj, value):
+		if obj != None:
+			obj.fields[self.field_name].value = value
+		else:
+			raise Exception('cannot assign value')
+
+
+class ModelMeta(type):
+	def __new__(cls, name, bases, dct):
+		# Model._fields is a class variable
+		dct['_fields'] = {}
+		klass = type.__new__(cls, name, bases, dct)
+		for a in dct:
+			if isinstance(dct[a], Field):
+				field = dct[a]
+				field.field_name = a
+				field.model_name = name
+				klass._fields[a] = field
+				setattr(klass, a, field_property(a))
+		return klass
+
+class Model(object):
+	__metaclass__ = ModelMeta
+
+	def __init__(self, instance=None, db=None):
+		self.db = db
+		self.table_name = self.__class__.__name__
+
+		# "fields" is an instance variable copied from class variable "_fields"
+		self.fields = {}
+		for n, f in self._fields.items():
+			field = Field(
+						f.field_type,
+						*f.validators,
+						value=f.value
+			)
+			field.field_name = f.field_name
+			field.model_name = f.model_name
+			self.fields[n] = field
+
+		# initialize from an instance, which is a dict or an object
+		if instance:
+			if isinstance(instance, dict):
+				for n, v in instance.items():
+					if n in self.fields:
+						setattr(self, n, v)
+			else:
+				for n in self.fields:
+					if hasattr(instance, n):
+						setattr(self, n, getattr(instance, n))
+
+	def _save_postgresql(self, names, values):
+		sql = 'INSERT INTO %s (%s) VALUES'%(self.table_name, ', '.join(names))
+		sql += ' ('+ ', '.join(['%s']*len(values)) +')'
+		return sql
+
+	# if self exists, save --> update
+	def save(self):
+		names = [ n for n,f in self.fields.items() ]
+		values = tuple( f.value for n,f in self.fields.items() )
+		sql = self._save_postgresql(names, values)
+		if self.db != None:
+			with self.db:
+				db.execute(sql, values)
+		else:
+			return sql, values
+
+	# Employee.update(('age', 20), ('name', 'John'), query)
+	# e.update(('age', 20), ('name', 'John'), key=('name',))
+	@classmethod
+	def update(self, expr):
+		''' update <table> set <column> = <expr> where <condition>'''
+		pass
+
+	@classmethod
+	def find(self, expr):
+		query = 'SELECT FROM %s WHERE %s;'
+
+	def validate(self):
+		for name, field in self.fields.items():
+			if field.eval() == False:
+				raise InvalidField('"%s" - %s' % (field.field_name, field.op.error))
+		return True
+
+	def to_dict(self):
+		return { name:field.value for name,field in self.fields.items() }
+
+	def to_json(self):
+		d = {name:field.serialize() for name,field in self.fields.items()}
+		return json.dumps(d)
+
+	def __str__(self):
+	 	return '\n'.join(str(field) for name,field in self.fields.items())
+
+	def __repr__(self):
+	 	return '\n'.join(repr(field) for name,field in self.fields.items())
---
+++ sal/validator.py
@@ -0,0 +1,298 @@
+import re
+
+##---------------------------------------------------------------------
+class Validator(object):
+	def __init__(self, name, b):
+		self.error = None
+		self.name = name
+		# In "a op b", b is the default second operand
+		self.b = b
+
+	def eval(self, result, a, b=None):
+		if result == False:
+			if b!=None:
+				self.error = '%s(%s, %s) = false' % (self.name, str(a), str(b))
+			else:
+				self.error = '%s(%s) = false' % (self.name, str(a))
+		return result
+
+	def sql(self):
+		raise NotImplementedError
+
+	def mongo(self):
+		raise NotImplementedError
+
+##---------------------------------------------------------------------
+## Database validators
+##---------------------------------------------------------------------
+class is_required(Validator):
+	def __init__(self):
+		super(is_required, self).__init__('is_required', None)
+
+	def __call__(self, value):
+		result = value is not None
+		if not result:
+			self.error = 'is_required(%s) = false' % str(value)
+		return result
+
+	def sql(self, x): return '(%s != NULL)' % x
+
+	def mongo(self, x): return {x : {'$exists' : True}}
+
+
+class is_eq(Validator):
+	def __init__(self,b=None):
+		super(is_eq, self).__init__('is_eq', b)
+
+	def __call__(self,a,b=None):
+		b = b if b!=None else self.b
+		return super(is_eq, self).eval(a==b, a, b)
+
+	def sql(self, x, y): return '(%s = %s)' % (x,y)
+
+	def mongo(self, x, y): return {x : y}
+
+
+class is_ne(Validator):
+	def __init__(self,b=None):
+		super(is_ne, self).__init__('is_ne', b)
+
+	def __call__(self,a,b=None):
+		b = b if b!=None else self.b
+		return super(is_ne, self).eval(a!=b, a, b)
+
+	def sql(self, x, y): return '(%s != %s)' % (x,y)
+
+	def mongo(self, x, y): return {x : {'$ne' : y}}
+
+
+class is_lt(Validator):
+	def __init__(self,b=None):
+		super(is_lt, self).__init__('is_lt', b)
+
+	def __call__(self,a,b=None):
+		b = b if b!=None else self.b
+		return super(is_lt, self).eval(a<b, a, b)
+
+	def sql(self, x, y): return '(%s < %s)' % (x,y)
+
+	def mongo(self, x, y): return {x : {'$lt' : y}}
+
+
+class is_le(Validator):
+	def __init__(self,b=None):
+		super(is_le, self).__init__('is_le', b)
+
+	def __call__(self,a,b=None):
+		b = b if b!=None else self.b
+		return super(is_le, self).eval(a<=b, a, b)
+
+	def sql(self, x, y): return '(%s <= %s)' % (x,y)
+
+	def mongo(self, x, y): return {x : {'$lte' : y}}
+
+
+class is_gt(Validator):
+	def __init__(self,b=None):
+		super(is_gt, self).__init__('is_gt', b)
+
+	def __call__(self,a,b=None):
+		b = b if b!=None else self.b
+		return super(is_gt, self).eval(a>b, a, b)
+
+	def sql(self, x, y): return '(%s > %s)' % (x,y)
+
+	def mongo(self, x, y): return {x : {'$gt' : y}}
+
+
+class is_ge(Validator):
+	def __init__(self,b=None):
+		super(is_ge, self).__init__('is_ge', b)
+
+	def __call__(self,a,b=None):
+		b = b if b!=None else self.b
+		return super(is_ge, self).eval(a>=b, a, b)
+
+	def sql(self, x, y): return '(%s >= %s)' % (x,y)
+
+	def mongo(self, x, y): return {x : {'$gte' : y}}
+
+
+class is_both(Validator):
+	def __init__(self):
+		super(is_both, self).__init__('is_both', None)
+
+	def __call__(self,a,b):
+		return super(is_both, self).eval(a and b, a, b)
+
+	def sql(self, x, y): return '(%s AND %s)' % (x,y)
+
+	def mongo(self, x, y): return {'$and' : [x, y]}
+
+
+class is_either(Validator):
+	def __init__(self):
+		super(is_either, self).__init__('is_either', None)
+
+	def __call__(self,a,b):
+		return super(is_either, self).eval(a or b, a, b)
+
+	def sql(self, x, y): return '(%s OR %s)' % (x,y)
+
+	def mongo(self, x, y): return {'$or' : [x, y]}
+
+
+class is_not(Validator):
+	def __init__(self):
+		super(is_not, self).__init__('is_not', None)
+
+	def __call__(self,a):
+		return super(is_not, self).eval(not a, a, None)
+
+	def sql(self, x): return '(NOT %s)' % x
+
+	def mongo(self, x): return {'$not' : x}
+
+
+class is_between(Validator):
+	def __init__(self, m, n):
+		self.m = m
+		self.n = n
+		super(is_between, self).__init__('is_between', None)
+
+	def __call__(self, value):
+		result = self.m <= value <= self.n
+		if not result:
+			self.error = 'is_between(%s, %s)(%s) = false' % \
+				(str(self.m),str(self.n),str(value))
+		return result
+
+	def sql(self, x, a, y): return '(%s <= %s AND %s <= %s)' % (x,a,a,y)
+
+	def mongo(self, x, a, y): return {'$and':[{x :{'$lte':a}}, {y:{'$gte':a}}]}
+
+
+class is_in(Validator):
+	def __init__(self, *things):
+		self.things = things
+		super(is_in, self).__init__('is_in', None)
+
+	def __call__(self, value):
+		result = value in self.things
+		if not result:
+			self.error = 'is_in(%s)(%s) = false' % (str(self.things), str(value))
+		return result
+
+	def sql(self, x, y): return '(%s in %s)' % (x,y)
+
+	def mongo(self, x, y): return {x : {'$in' : y}}
+
+
+class is_notin(Validator):
+	def __init__(self, *things):
+		self.things = things
+		super(is_in, self).__init__('is_notin', None)
+
+	def __call__(self, value):
+		result = value in self.things
+		if not result:
+			self.error = 'is_notin(%s)(%s) = false' % (str(self.things), str(value))
+		return result
+
+	def sql(self, x, y): return '(NOT (%s IN %s))' % (x,y)
+
+	def mongo(self, x, y): return {x : {'$nin' : y}}
+
+
+#--------------------------------------------------------------------------
+
+class combine_validators(Validator):
+	def __init__(self, validators):
+		self.validators = validators
+		super(combine_validators, self).__init__('combine_validators',None)
+
+	def __call__(self, value):
+		for validator in self.validators:
+			if validator(value) == False:
+				self.error = validator.error
+				return False
+		return True
+
+class is_value(Validator):
+	def __init__(self):
+		super(is_value, self).__init__('is_value', None)
+
+	def __call__(self, value, converter):
+		return converter.value(value) if hasattr(converter,'value') else value
+
+#--------------------------------------------------------------------------
+class is_length(Validator):
+	def __init__(self, m, n=None):
+		self.m = m
+		self.n = n
+		super(is_length, self).__init__('is_legnth', None)
+
+	def __call__(self, value):
+		if self.n==None:
+			result = len(value)==self.m
+			if not result:
+				self.error = 'is_length(%s) != len(%s)' % (self.m, value)
+		else:
+			result = self.m <= len(value) <= self.n
+			if not result:
+				self.error = 'is_length(%s, %s) != len(%s)' % \
+					(self.m, self.n, value)
+		return result
+
+
+class is_month(is_in):
+	def __init__(self):
+		super(is_month, self).__init__( \
+			('January','February','March','April','May','June', \
+				'July','August','September','October','November','December')
+		)
+
+class is_weekday(is_in):
+	def __init__(self):
+		super(is_weekday, self).__init__(\
+			('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
+		)
+
+class is_email(Validator):
+	EMAIL_REGEX = re.compile(
+		# dot-atom
+		r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"
+		# quoted-string
+		r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016'
+		r'-\177])*"'
+		# domain
+		r')@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$',
+		re.IGNORECASE
+	)
+	def __init__(self):
+		super(is_email, self).__init__('is_email', None)
+
+	def __call__(self, value):
+		result = True if is_email.EMAIL_REGEX.match(value) else False
+		if not result:
+			self.error = 'is_email(%s) = false' % str(value)
+		return result
+
+class is_url(Validator):
+	URL_REGEX = re.compile(
+		r'^https?://'
+		r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'
+		r'localhost|'
+		r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+		r'(?::\d+)?'
+		r'(?:/?|[/?]\S+)$', re.IGNORECASE
+	)
+	def __init__(self):
+		super(is_url, self).__init__('is_url', None)
+
+	def __call__(self):
+		result = True if URL_REGEX.match(value) else False
+		if not result:
+			self.error = 'is_url(%s) = false' % str(value)
+		return result
+
